69af36a494d8cebb3fa16c83cc8b95c8531d690c
dragonav <daniliandan@gmail.com>
2024-10-08 10:51:41 +0200

    Specified that s31600 will add 'Subtractor'

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 public class Main {
+    //TODO we need to add the missing classes!
+    //I will add "adder" and s31600 will add 'Subtractor'
+
+
     public static void main(String[] args) {
     Adder adder = new Adder();
     System.out.println(adder.add(1,2));
